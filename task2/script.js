aceaa671027455e478e9902a7de28f25a19a4d8d
ErShivnandan <ec1912@global.org.in>
2024-09-27 14:11:28 +0530

    responsiveness completed

--- task2/script.js
+++ task2/script.js
@@ -1,11 +1,28 @@
-const hamburger = document.querySelector('.hamburger');
-const sidebar = document.querySelector('.sidebar');
-const closeBtn = document.querySelector('.close-btn');
+const hamburger = document.querySelector(".hamburger");
+const sidebar = document.querySelector(".sidebar");
+const closeBtn = document.querySelector(".close-btn");
 
-hamburger.addEventListener('click', () => {
-  sidebar.style.left = '0';
+hamburger.addEventListener("click", () => {
+  sidebar.style.left = "0";
 });
 
-closeBtn.addEventListener('click', () => {
-  sidebar.style.left = '-250px';
+closeBtn.addEventListener("click", () => {
+  sidebar.style.left = "-250px";
 });
+
+function getFormattedDate(date) {
+  const options = { month: "long", day: "numeric" };
+  return date.toLocaleDateString(undefined, options);
+}
+
+const today = new Date();
+
+const tenDaysLater = new Date(today);
+tenDaysLater.setDate(today.getDate() + 10);
+
+const formattedToday = getFormattedDate(today);
+const formattedTenDaysLater = getFormattedDate(tenDaysLater);
+
+document.getElementById(
+  "dateRangeInput"
+).placeholder = `${formattedToday} ~ ${formattedTenDaysLater}`;
